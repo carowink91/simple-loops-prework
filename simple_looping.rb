3f778411cd89d2ca5ab3b74ab2489c83fdfb0fd2
angelica winkler <caw400@nyu.edu>
2018-06-25 23:38:52 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,11 +11,18 @@ counter = 0
   end
 end
 
-def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
-  phrase = "Welcome to Flatiron School's Web Development Course!"
 
+
+
+def times_iterator(number_of_times)
+  number_of_times.times do
+    puts "Welcome to Flatiron School's Web Development Course!"
+  end
 end
+
+
+
+
 
 def while_iterator(number_of_times)
   # code your solution here using the "while" keyword
