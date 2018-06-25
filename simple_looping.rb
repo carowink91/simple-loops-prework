10c17878dfce5178fbd3841678778d219856e786
angelica winkler <caw400@nyu.edu>
2018-06-25 23:37:00 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,7 +1,7 @@
 # REMEMBER: print your output to the terminal using 'puts'
-counter = 0
 
 def loop_iterator(number_of_times)
+counter = 0
   loop do
     counter = counter + 1
     puts "Welcome to Flatiron School's Web Development Course!"
